86bbd1c0cd3629838f90b46d29ba76bd2818df2a
Eugene Budun <evhen.budun@monobank.ua>
2025-05-04 21:02:37 +0300

    Upd

--- example.py
+++ example.py
@@ -2,6 +2,6 @@ import os
 from short_link_sdk import sdk
 
 client = sdk.ShortLinkClient(os.getenv("SHORT_LINK_TOKEN"))
-resp = client.create_link("https://google.com")
+short_link = client.create_link("https://google.com")
 
-print(resp['Url'])
+print(short_link)
