024b3b12d235ddb5a3068c85986bf9072349fffe
Eugene Budun <evhen.budun@monobank.ua>
2025-05-04 22:13:30 +0300

    Upd

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="short_link_sdk",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "requests",
